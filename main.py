7cd50e2176bdbb36c5ad87e114912057f928ca98
susiaguilar <susanjoaguilar@gmail.com>
2023-08-07 20:07:13 -0600

    making the cards

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ suits = ["hearts", "diamonds", "spades", "clubs"]
 ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
 for suit in suits:
-    print([suit, ranks[0]])
+    for rank in ranks:
+        print([suit, rank])
     
