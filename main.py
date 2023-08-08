c435a24c50df6dd8bc8a55316f99dc1500bf9c13
susiaguilar <susanjoaguilar@gmail.com>
2023-08-07 20:14:04 -0600

    added random and shuffled cards

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import random
+
 cards = []
 suits = ["hearts", "diamonds", "spades", "clubs"]
 ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
 for suit in suits:
     for rank in ranks:
-        print([suit, rank])
+        cards.append([suit, rank])
+random.shuffle(cards)      
+print(cards)
+
     
