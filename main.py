028bdc94ef7eb583cc5ddf58730899af91964a39
susiaguilar <susanjoaguilar@gmail.com>
2023-08-07 19:55:03 -0600

    just getting started

---
+++ main.py
@@ -0,0 +1,10 @@
+suits = ["hearts", "diamonds", "spades", "clubs"]
+suit = suits[0]
+rank = "K"
+value = 10
+
+print("Your card is: " + rank + " of " + suit)
+
+for suit in suits:
+    print(suit)
+    
